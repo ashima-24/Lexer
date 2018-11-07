115edbe95b7b49fee8afe5d9623e39dee5712a2d
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-07 23:43:30 +0000

    lexer program

--- lex.cpp
+++ lex.cpp
@@ -8,8 +8,16 @@ for(i=0;i<10;i++)
 {
 cin>>c[i];
 switch(c[i])
-	case 'a': cout<<"true";
-		 if(c[i]=='a') {cout<<"\n position no"<<i ;}
-		    	}
+	case 'a': if(c[i]=='auto'||'break'||'case'||'char'||'const'||'continue'||
+	 'default'||'do'||'double'||'else'||'enum'||'extern'||'float'||'for'||'goto'||'if'
+	 ||'inline'||'int'||'long'||'register'||'restrict'||'return'||'short'||'signed'||
+	 'sizeof'||'static'||'struct'||'switch'||'typedef'||'union'||
+'unsigned'||'void'||'volatile'||'while'||' _Alignas'||' _Alignof'||' _Atomic'||' _Bool'||
+'_Complex'||' _Generic'||' _Imaginary'||' _Noreturn'||' _Static_assert'||' _Thread_local')
+{
+cout<<"keyword";
+}
+else cout<<"other token";
+}
 
 }
