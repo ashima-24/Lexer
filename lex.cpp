87d5e8fdea2e0b75a38e04d198021e5e73fca9c3
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-07 22:38:44 +0000

    lexer program

--- lex.cpp
+++ lex.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main ()
 {
-
-cout<<"tokens \n";
+	int i;
+char c[10];
+for(i=0;i<10;i++)
+{
+cin>>c[i];
+switch(c[i])
+	case 'a': cout<<"true";
+		 if(c[i]=='a') {cout<<"\n position no"<<i ;}
+		    	}
 
 }
