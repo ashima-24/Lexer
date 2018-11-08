3528895eb30ebaa60a8350c916394a997f3c0f06
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-08 00:52:53 +0000

    lexer program

--- lex.cpp
+++ lex.cpp
@@ -1,23 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
+
+/*string keywords[] = [
+	"auto", 
+	"break"
+];
+
+char *keywords1 = "case";
+*/
+
+char keywords[] = {"switch","auto"};
+
+//if (kewords[0] == keywords1 || kewords[1] == kewords2))
 int main ()
 {
 	int i;
-char c[10];
-for(i=0;i<10;i++)
-{
-cin>>c[i];
-switch(c[i])
-	case 'a': if(c[i]=='auto'||'break'||'case'||'char'||'const'||'continue'||
-	 'default'||'do'||'double'||'else'||'enum'||'extern'||'float'||'for'||'goto'||'if'
-	 ||'inline'||'int'||'long'||'register'||'restrict'||'return'||'short'||'signed'||
-	 'sizeof'||'static'||'struct'||'switch'||'typedef'||'union'||
-'unsigned'||'void'||'volatile'||'while'||' _Alignas'||' _Alignof'||' _Atomic'||' _Bool'||
-'_Complex'||' _Generic'||' _Imaginary'||' _Noreturn'||' _Static_assert'||' _Thread_local')
-{
-cout<<"keyword";
-}
-else cout<<"other token";
-}
+	char c[10];
+	for(i=0;i<10;i++)
+	{
+		cin>>c[i];
+		switch(c[i])
+		case 'a' :  
+		case's' : 	while(!='\0'){
+				
+				
+				}
+			cout<<"keyword";
+				
+			 
+			  
+	}
 
 }
