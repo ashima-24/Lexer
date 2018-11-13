01dc1f91eba7a60feb2d62caafc641cc356223c6
ashima-24 <jindal241995@gmail.com>
2018-11-13 23:21:42 +0100

    lexer

--- lex.cpp
+++ lex.cpp
@@ -3,37 +3,19 @@
 
 using namespace std;
 
-/*string keywords[] = [
-	"auto", 
-	"break"
-];
 
-char *keywords1 = "case";
-*/
-
-char keywords[] = {"switch","auto"};
+//char keywords[] = {"switch","auto"};
 char keywrd[25];
 //if (kewords[0] == keywords1 || kewords[1] == kewords2))
 int main ()
 {
-	int i;
-	char c[10];
-	for(i=0;i<10;i++)
-	{
-		cin>>c[i];
-		switch(c[i])
-		case 'a' :  
-		case's' : 	while(c[i]!='\0'||' ')
-				{
-				
-				keywrd[i]=keywords[i];
-				i++;
-				}
-				if(keywrd=="auto"||"switch")
-			cout<<"keyword";
-				
-			 
-			  
-	}
+    std::ifstream file "tokenize.txt";
+    char c; 
+    while(file.get(c))
+    {    std::cout<<c;
+    
+    
+file.close();
+    }
 
 }
