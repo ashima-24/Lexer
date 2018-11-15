170e5f7bb2d09f49852df3a5511e7c674ca745fa
ashima-24 <jindal241995@gmail.com>
2018-11-15 21:30:38 +0100

    lexer start

--- lexer.cpp
+++ lexer.cpp
@@ -1,53 +1,25 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+
 using namespace std;
-int main()
-
-
-{
-	int w,b,t,n;
-float f,c;
-cout<<"hello,";
-cout<<"world \c";
-cout<<"\n";
-/*cin>>f;
-c=0.56*f-32;
-cout<<"temp in celsius"<<c;
-
-for( f;f>=0;f--)
-{
-c=0.56*f-32;
-cout<<"celsius temp"<<\n<<c;
-} 
-char ch;
-cin>>ch;
-if(ch!=EOF)
-	cout<<"t";
-	else cout<<"n";
-cout<<EOF<<"\n"; */
-char test[13];
-int i,j;
-for(i =0;i<13;i++)
-cin>>test[i];
-for( j=0;j!=EOF;j++)
-{
-if(test[j]=='\n')
-	n++;
-else if(test[j]=='\t')
-	t++;
-else if(test[j]==' ')
-	b++;
-else  w++;
+ int main()
+{ char ch;
+    std::ifstream fs("--tokenize.txt");
+if(fs)
+{ while(fs.get(ch))
+   // std::cout<<ch<<"\n";
 }
-cout<<"wrd"<<w;
-cout<<"gap"<<b;
-cout<<"tab"<<t;
-cout<<"line"<<n;
+    if(fs.eof())
+    std::cout<<"end reached";
 
 
+
+
+fs.close();
+
+
+return 0;
 
 }
-
-
-
-
 
